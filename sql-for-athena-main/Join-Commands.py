8b3b2b127023c328165a60bba83190ce44ee3da8
OhWhale515 <sterlinggreen515@gmail.com>
2023-04-10 11:03:56 -0400

    merges

--- sql-for-athena-main/Join-Commands.py
+++ sql-for-athena-main/Join-Commands.py
@@ -15,3 +15,13 @@ suffixes = ["_l", "_r"]
 
 # provide the type of join tot he table
 indicator="indicator_col"
+
+# Merge the two dataframes on loannumber
+merged_df = pd.merge(preds_df, filter_df, on="loannumber", how="outer")
+
+# Filter rows where loannumber is not in filter_df or prediction change > 10%
+filtered_df = merged_df[~merged_df["loannumber"].isin(filter_df["loannumber"]) | (merged_df["prediction_change"] > 0.1)]
+
+# Select only the relevant columns and output the rows
+output_df = filtered_df[["loannumber", "prediction", "prediction_change"]]
+print(output_df)
